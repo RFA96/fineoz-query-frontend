2d314988d7295a8fc8b6f92ec9c9ecb5f4e75632
lutfi-hakim <21lutfihakim@gmail.com>
2020-08-13 21:45:48 +0700

    list card

---
+++ pages/list_card.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <title>Monitoring</title>
+        <?php include "../partials/assets_css.php"; ?>
+
+    </head>
+
+    <body class="bg-white">
+        <?php include "../partials/topnav.php"; ?>
+
+        <div class="container">
+            <div class="row justify-content-center py-4">
+                <div style="width: 20rem;">
+                    <div class="card card-stats mr-4">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <span class="h2 font-weight-bold mb-0">INDIHOME</span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape text-white rounded-circle shadow">
+                                        <img src="../assets/img/brand/image-logo.png" style="width: 4rem">
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="mt-3 mb-0 text-sm">
+                                <span class="text-red mr-2"><i class="fa fa-database"></i> 348</span>
+                                <span class="text-nowrap mr-4">Data Field</span>
+                                <a href="detail_list_data.php" class="text-right">Details <i class="ni ni-bold-right"></i></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div style="width: 20rem;">
+                    <div class="card card-stats mr-4">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col">
+                                    <span class="h2 font-weight-bold mb-0">INDOSAT</span>
+                                </div>
+                                <div class="col-auto">
+                                    <div class="icon icon-shape text-white rounded-circle shadow">
+                                        <img src="../assets/img/brand/image-logo.png" style="width: 4rem">
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="mt-3 mb-0 text-sm">
+                                <span class="text-red mr-2"><i class="fa fa-database"></i> 348</span>
+                                <span class="text-nowrap">Data Field</span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "../partials/assets_js.php"; ?>
+
+    </body>
+
+</html>
